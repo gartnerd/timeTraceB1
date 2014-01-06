4a6295e912785aa39232a2868192791e0f2c0b4e
David Gartner <gartnerd@gmail.com>
2014-01-05 20:09:47 -0700

    updated logic for multiple time clocks

--- timeTrace.py
+++ timeTrace.py
@@ -5,7 +5,7 @@ import time
 import datetime
 
 from PySide import QtCore, QtGui
-from timeTraceUI import Ui_MainWindow
+from timeTraceUI2 import Ui_MainWindow
 
 
 class timers(QtGui.QMainWindow):
@@ -13,23 +13,56 @@ class timers(QtGui.QMainWindow):
         super(timers, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.lcdTimer = QtCore.QTimer()
-        self.lcdElapsedTimer = QtCore.QElapsedTimer()
+
+        self.clock1 = StartStopClock()
+        self.deltatime = self.clock1.lcdElapsedTimer.elapsed()
+        self.clock1delta = ElapsedTime()
+        self.clock1delta.elapsedTime(self.deltatime)
+
+        self.clock2 = StartStopClock()
 
         #buttons
-        QtCore.QObject.connect(self.ui.exit, QtCore.SIGNAL("clicked()"), self.close)
-        QtCore.QObject.connect(self.ui.stop, QtCore.SIGNAL("clicked()"), self.stop_clock)
-        QtCore.QObject.connect(self.ui.start, QtCore.SIGNAL("clicked()"), self.start_clock)
+        QtCore.QObject.connect(self.ui.exit,    QtCore.SIGNAL("clicked()"), self.close)
+        QtCore.QObject.connect(self.ui.exit_2,  QtCore.SIGNAL("clicked()"), self.close)
+        QtCore.QObject.connect(self.ui.stop,    QtCore.SIGNAL("clicked()"), self.clock1.stop_clock)
+        QtCore.QObject.connect(self.ui.start,   QtCore.SIGNAL("clicked()"), self.clock1.start_clock)
+        QtCore.QObject.connect(self.ui.stop_2,  QtCore.SIGNAL("clicked()"), self.clock2.stop_clock)
+        QtCore.QObject.connect(self.ui.start_2, QtCore.SIGNAL("clicked()"), self.clock2.start_clock)
 
-        QtCore.QObject.connect(self.lcdTimer, QtCore.SIGNAL("timeout()"), self.updtTime)
+        QtCore.QObject.connect(self.clock1.lcdTimer, QtCore.SIGNAL("timeout()"), self.updtTime)
+        QtCore.QObject.connect(self.clock2.lcdTimer, QtCore.SIGNAL("timeout()"), self.updtTime2)
+
 
         QtCore.QMetaObject.connectSlotsByName(self)
 
-    def constant(self):
+
+    def single(self):
         """
-        Start the constant timer
+        run singleShot timer after button push
         """
-        self.ctimer.start(1000)
+        self.stimer.singleShot(1000, self.singleUpdate)
+
+
+    def updtTime(self):
+        currentTime1 = datetime.timedelta(seconds=self.clock1.lcdElapsedTimer.elapsed()/1000)
+        self.ui.lcdNumber.display(str(currentTime1))
+
+    def updtTime2(self):
+        #currentTime = QtCore.QDateTime.currentDateTime().toString('hh:mm:ss')
+        currentTime2 = datetime.timedelta(seconds=self.clock2.lcdElapsedTimer.elapsed()/1000)
+        self.ui.lcdNumber_2.display(str(currentTime2))
+
+class ElapsedTime(object):
+    def elapsedTime(self,deltaTime):
+        self.delT = datetime.timedelta(seconds=deltaTime)
+        #return self.delT
+        #self.lcdDisplay.display(str(self.delT))
+
+
+class StartStopClock(object):
+    def __init__(self):
+        self.lcdTimer = QtCore.QTimer()
+        self.lcdElapsedTimer = QtCore.QElapsedTimer()
 
     def start_clock(self):
         self.lcdElapsedTimer.start()
@@ -38,30 +71,6 @@ class timers(QtGui.QMainWindow):
     def stop_clock(self):
         self.lcdTimer.stop()
 
-    def constantUpdate(self):
-        """
-        slot for constant timer timeout
-        """
-        pass
-
-    def single(self):
-        """
-        run singleShot timer after button push
-        """
-        self.stimer.singleShot(1000, self.singleUpdate)
-
-    def singleUpdate(self):
-        """
-        Slot for singleShot timer timeout
-        """
-        pass
-
-    def updtTime(self):
-        #currentTime = QtCore.QDateTime.currentDateTime().toString('hh:mm:ss')
-        currentTime = datetime.timedelta(seconds=self.lcdElapsedTimer.elapsed()/1000)
-        self.ui.lcdNumber.display(str(currentTime))
-        #self.ui.myTimeDisplay.display(currentTime)
-        #print currentTime
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
